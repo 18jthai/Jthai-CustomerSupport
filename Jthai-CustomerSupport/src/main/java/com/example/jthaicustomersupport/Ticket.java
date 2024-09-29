fa149bba1b12ba959bd43841ccef7d9e6324e4b1
18jthai <129327835+18jthai@users.noreply.github.com>
2024-09-29 16:01:50 -0500

    UpdateTicket

--- Jthai-CustomerSupport/src/main/java/com/example/jthaicustomersupport/Ticket.java
+++ Jthai-CustomerSupport/src/main/java/com/example/jthaicustomersupport/Ticket.java
@@ -1,4 +1,52 @@
 package com.example.jthaicustomersupport;
 
+import java.util.ArrayList;
+
 public class Ticket {
+    private String name;
+    private String subject;
+    private String bodyOfTicket;
+    private ArrayList attachments;
+
+    public Ticket(String name, String subject, String bodyOfTicket, ArrayList attachments) {
+        this.name = name;
+        this.subject = subject;
+        this.bodyOfTicket = bodyOfTicket;
+        this.attachments = attachments;
+    }
+
+    public Ticket() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getBodyOfTicket() {
+        return bodyOfTicket;
+    }
+
+    public ArrayList getAttachments() {
+        return attachments;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public void setBodyOfTicket(String bodyOfTicket) {
+        this.bodyOfTicket = bodyOfTicket;
+    }
+
+    public void setAttachments(ArrayList attachments) {
+        this.attachments = attachments;
+    }
 }
